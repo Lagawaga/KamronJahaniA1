852a322f95a408a5f62a01afd38679180854da97
Lagawaga <kamronjahani@gmail.com>
2018-09-20 03:22:21 -0400

    implemented Dealer Class

---
+++ src/test/java/core/DealerTest.java
@@ -0,0 +1,86 @@
+package core;
+
+import junit.framework.TestCase;
+
+public class DealerTest extends TestCase{
+	
+	public void testStand(){
+		Deck deck = new Deck();
+		Dealer dealer = new Dealer(new Deck());
+		deck.newDeck();
+		dealer.hit(deck);
+		int x = dealer.deckTotal();
+		dealer.stand();
+		assertEquals(x, dealer.deckTotal());
+		
+	}
+	
+	public void testHit() {
+		Dealer dealer = new Dealer(new Deck());
+		Deck deck = new Deck();
+		deck.newDeck();
+		int x = dealer.deckTotal();
+		dealer.hit(deck);
+		assertTrue(x < dealer.deckTotal());
+	}
+	
+	public void testHitRepeat() {
+		Dealer dealer = new Dealer(new Deck());
+		Deck deck = new Deck();
+		deck.newDeck();
+		int x = dealer.deckTotal();
+		dealer.hit(deck);
+		dealer.hit(deck);
+		dealer.hit(deck);
+		int y = dealer.deckTotal();
+		assertTrue(x < y);
+	}
+	
+	public void testInitialBlackJack() {
+		Dealer dealer = new Dealer(new Deck());
+		dealer.add(new Card(1,1));
+		dealer.add(new Card(11,2));
+		assertEquals(true, dealer.initialBlackJack());
+	}
+	
+	
+	
+	public void testDealerHit() {
+		Dealer dealer = new Dealer(new Deck());
+		Deck deck = new Deck();
+		deck.addCard(new Card(1,3));
+		deck.addCard(new Card(6,2));
+		assertEquals(true, dealerHit());
+		
+	}
+	
+	public void testDealerStand() {
+		Dealer dealer = new Dealer(new Deck());
+		Deck deck = new Deck();
+		deck.addCard(new Card(1,3));
+		deck.addCard(new Card(5,2));
+		assertEquals(true, dealerStand());
+	}
+	
+	public void testDealerBust() {
+		Dealer dealer = new Dealer(new Deck());
+		Deck deck = new Deck();
+		deck.addCard(new Card(1,3));
+		deck.addCard(new Card(1,2));
+		assertEquals(true, dealerBust());
+	}
+	
+	
+	
+	public void testDeckTotal() {
+		Deck deck1 = new Deck();
+		deck1.newDeck();
+		deck1.shuffle();
+		Deck deck2 = new Deck();
+		deck2.addCard(deck1.getCard(0));
+		deck2.addCard(deck1.getCard(1));
+		
+		assertEquals((deck2.getCard(0).getValue()+deck2.getCard(1).getValue()), deck2.deckTotal());
+	}
+
+}
