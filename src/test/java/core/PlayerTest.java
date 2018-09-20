8d5628a2b085b648c542f1fac66ada24476e450e
Lagawaga <kamronjahani@gmail.com>
2018-09-20 02:47:23 -0400

    minor corrections

--- src/test/java/core/PlayerTest.java
+++ src/test/java/core/PlayerTest.java
@@ -7,37 +7,48 @@ public class PlayerTest extends TestCase{
 	public void testStand(){
 		Deck playingDeck = new Deck();
 		Player player = new Player(new Deck());
-		player.drawCard(playingDeck);
-		int x = player.getValue();
+		playingDeck.newDeck();
+		player.hit(playingDeck);
+		int x = player.deckTotal();
 		player.stand();
-		assertEquals(x, player.getValue);
+		assertEquals(x, player.deckTotal());
 		
 	}
 	
 	public void testHit() {
 		Player player = new Player(new Deck());
 		Deck playingDeck = new Deck();
-		int x = player.getValue();
+		playingDeck.newDeck();
+		int x = player.deckTotal();
 		player.hit(playingDeck);
-		assertTrue(x < player.getValue());
+		assertTrue(x < player.deckTotal());
 	}
 	
 	public void testHitRepeat() {
 		Player player = new Player(new Deck());
 		Deck playingDeck = new Deck();
-		int x = player.getValue();
+		playingDeck.newDeck();
+		int x = player.deckTotal();
 		player.hit(playingDeck);
 		player.hit(playingDeck);
 		player.hit(playingDeck);
-		int y = player.getValue();
+		int y = player.deckTotal();
 		assertTrue(x < y);
 	}
 	
 	public void testInitialBlackJack() {
 		Player player = new Player(new Deck());
 		player.add(new Card(1,1));
-		player.add(new Card(1,10));
-		assertEquals(true, initialBlackJack());
+		player.add(new Card(11,2));
+		System.out.println(player.toString());
+		System.out.println(player.deckTotal());
+		assertEquals(true, player.initialBlackJack());
+	}
+	
+	public void testNewDeck(){
+		Deck deck = new Deck();
+		deck.newDeck();
+		assertEquals(52, deck.numCards());
 	}
 	
 	
